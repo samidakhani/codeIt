a8d8476b28a312dfcbceb3bf1b439910e64d3ae2
Sami Dakhani <samidakhani@gmail.com>
2016-11-09 23:24:07 -0500

    Adding an optimised method to twoSums

--- src/main/java/com/dakhani/learn/TwoSum.java
+++ src/main/java/com/dakhani/learn/TwoSum.java
@@ -1,19 +1,39 @@
 package com.dakhani.learn;
 
+import java.util.Arrays;
+
 /**
- * https://leetcode.com/problems/two-sum/
+ * https://leetcode.com/problems/two-sum/ Result:Your runtime beats 81.68% of
+ * java submissions.
  * 
  * @author Sami Dakhani Created on Nov 9, 2016
  *
  */
 public class TwoSum {
 
+	private static int findIndexOf(final int[] nums, final int key,
+			boolean second) {
+
+		int index = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (key == nums[i] && !second) {
+				index = i;
+				break;
+			} else if (key == nums[i] && second) {
+				second = false;
+			}
+		}
+
+		return index;
+	}
+
 	public static void main(final String[] args) {
 
-		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
+		int[] nums = { 3, 2, 4 };
+		int target = 6;
 
-		int[] result = twoSum(nums, target);
+		int[] result = twoSumOptimised(nums, target);
 
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
@@ -32,6 +52,38 @@ public class TwoSum {
 				}
 			}
 
+		}
+
+		return result;
+	}
+
+	public static int[] twoSumOptimised(final int[] nums, final int target) {
+
+		int[] result = new int[2];
+		int[] sortedNums = nums.clone();
+		Arrays.sort(sortedNums);
+
+		int i = 0;
+		int j = sortedNums.length - 1;
+
+		while (i <= j) {
+
+			if (sortedNums[i] + sortedNums[j] == target) {
+
+				int firstElem = sortedNums[i];
+				int secondElem = sortedNums[j];
+
+				result[0] = findIndexOf(nums, firstElem, false);
+				result[1] = findIndexOf(nums, secondElem,
+						firstElem == secondElem);
+
+				break;
+
+			} else if (sortedNums[i] + sortedNums[j] < target) {
+				i++;
+			} else {
+				j--;
+			}
 		}
 
 		return result;
